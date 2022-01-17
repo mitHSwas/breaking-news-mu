ce1028f28bb5dda6c9df9c92e8a4811ce39d8e44
Mithun Biswas <mithunbiswasbu92@gmail.com>
2022-01-17 12:37:15 +0600

    axios is useing to load data from news api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import Button from '@mui/material/Button'
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Chart from './components/Chart/Chart';
 import News from './components/News/News';
 
 function App() {
   const [articles, setArticles] = useState([])
+  // useEffect( () => {
+  //   const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-12-16&sortBy=publishedAt&apiKey=59d7f339afa84dc9ab5b5b2b027994d7';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => console.log(data.articles))
+  // },[])
   useEffect( () => {
     const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-12-16&sortBy=publishedAt&apiKey=59d7f339afa84dc9ab5b5b2b027994d7';
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
-  },[])
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
   return (
     <div>
       {
         articles.map( article => <News article={article}></News>)
       }
+      <Chart></Chart>
     </div>
   );
 }
